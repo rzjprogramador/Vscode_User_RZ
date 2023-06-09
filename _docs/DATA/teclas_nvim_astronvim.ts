e00c5dd0432a3a860f1cac067c0ea19ca4a26a91
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-09 19:49:38 -0300

    todo: atalhos importantes default astro

--- _docs/DATA/teclas_nvim_astronvim.ts
+++ _docs/DATA/teclas_nvim_astronvim.ts
@@ -1,7 +1,44 @@
-const teclas_nvim_astronvim = {
-	tab: 'Tab',
-	funcionais: 'F10',
-	control: 'C',
-	shift: 'S',
-	alt: '<A> , OBS: TEM QUE PULA RO ABRRA DA MEMORIA -> , ":m .+1<CR>==", opts)',
-}
+
+const teclas =[
+	{
+		categoria: 'teclas',
+		contexto: '',
+		titulo: ''
+		codigo: '',
+		conceito: 'no <???> as variaveis de teclas começam com <???>',
+		obs: '#nulo',
+	},
+	{
+		categoria: 'teclas',
+		contexto: 'astronvim',
+		titulo: 'teclas funcionais',
+		codigo: 'F10 ou numero da F<numero>',
+		conceito: 'no astronvim as variaveis de teclas começam com Maiusculo.',
+		obs: '#nulo',
+	},
+	{
+		categoria: 'teclas',
+		contexto: 'astronvim',
+		titulo: 'control',
+		codigo: 'C',
+		conceito: 'no astronvim as variaveis de teclas começam com Maiusculo.',
+		obs: '#nulo',
+	},
+	{
+		categoria: 'teclas',
+		contexto: 'astronvim',
+		titulo: 'shift',
+		codigo: 'S',
+		conceito: 'no astronvim as variaveis de teclas começam com Maiusculo.',
+		obs: '#nulo',
+	},
+	{
+		categoria: 'teclas',
+		contexto: 'astronvim',
+		titulo: 'alt',
+		codigo: '<A> , conceito: 'no astronvim as variaveis de teclas começam com Maiusculo.',
+		OBS: TEM QUE PULA RO ABRRA DA MEMORIA -> , ":m .+1<CR>==", opts)',
+		conceito: 'no astronvim as variaveis de teclas começam com Maiusculo.',
+		obs: '#nulo',
+	},
+]
