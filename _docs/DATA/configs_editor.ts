2f9a6a1b98a2167182e77f9ccd3f4d28f8eae998
@ReinaldoReizao <60945992+reinaldoreizao@users.noreply.github.com>
2023-06-10 22:43:07 -0300

    feat: configs astronvim

---
+++ _docs/DATA/configs_editor.ts
@@ -0,0 +1,18 @@
+
+const configs_editor = [
+  {
+    categoria: "nvim_astronvim",
+    titulo: "local arquivos share e backups astronvim",
+    contexto: "~/.local/share/nvim/lazy/lazy.nvim/lua/lazy/core/",
+  },
+  {
+    categoria: "nvim_astronvim",
+    titulo: "cache astronvim",
+    contexto: "~/.local/share/nvim/lazy/lazy.nvim/lua/lazy/core/cache",
+  },
+  {
+    categoria: "nvim_astronvim",
+    titulo: "backup arquivo swp swpa astronvim",
+    contexto: "~/.local/state/nvim/swap/",
+  },
+]
